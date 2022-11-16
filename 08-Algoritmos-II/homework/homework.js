7761f91231d10b8c2849e64a3d67a708525f1358
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2022-11-16 18:13:23 -0500

    second instance of the homwork

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,55 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
 
+  let pivot = array[Math.floor(Math.random() * array.length)]
+  let left = [];
+  let right = [];
+  let equals = [];
+  
+  for(let i = 0; i < array.length; i++) {
+    
+    if(array[i] < pivot){
+      left.push(array[i]);
+    }else if(array[i] > pivot){
+      right.push(array[i]);
+    }
+    else{
+      equals.push(array[i]);
+    }
+  }
+  return quickSort(left).concat(equals).concat(quickSort(right));
+}
+
+
+
+function split(array){
+
+  let middle = Math.floor(array.length/2)
+  var left = array.slice(0, middle)
+  var right = array.slice(middle)
+
+  return [left, right];
+
+}
+
+function merge(left, right){
+  
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let array = [];
+
+  while(leftIndex < left.length && rightIndex < right.length){
+    if(left[leftIndex] < right[rightIndex]){
+      array.push(left[leftIndex]);
+      leftIndex++;    
+    }else{
+      array.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return array.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 function mergeSort(array) {
@@ -14,6 +62,23 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
+
+  let div = split(array);
+  let left = div[0];
+  let right = div[1];
+
+  return merge(mergeSort(left), mergeSort(right))
+
+  // for(let i = 0; i < array.length; i++) {
+  //   for(let j = 1; j < array.length; j++) {
+  //     if(array[i] > array[j]) {
+  //       let temp = array[i];
+  //       array[i] = array[j];
+  //       array[j] = temp;
+  //     }
+  //   }
+  // }
 
   //okey probemos ahora
 }
